dc0c681534dbad4df8e80a45670418cc558d4260
Roman56662 <77270655+Roman56662@users.noreply.github.com>
2021-01-28 12:46:15 +0300

    load

--- typerace-hooks/src/App.js
+++ typerace-hooks/src/App.js
@@ -1,19 +1,52 @@
-import React, {useState} from 'react';
-import {render} from 'react-dom'
+import React, {useState, useEffect} from 'react';
 
-const App = () => {
+
+const App = () => { 
+    const SNIPPETS = [
+        "Bears, beets, battlestar galactica",
+        "what's Forrest Gump's password? 1Forrest1",
+        "где любят тусоваться программисты? Foo Bar"]
+    const INITIAL_GAME_STATE = { victory: false, startTime: null, endTime: null };
+    const [snippet,setSnippet] = useState('');
     const [userText, setUserText] = useState('');
-
+    const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
+    useEffect(() =>{
+        if (gameState.victory) document.title = 'Vistory!';
+    });
     const updateUserText = event => {
         setUserText(event.target.value);
-        console.log('cerrent userText',userText);
+        if (event.target.value === snippet) {
+            setGameState({
+                ...gameState,
+                victory:true,
+                endTime:new Date().getTime() - gameState.startTime
+            })
+        }
     }
+    const chooseSnippet = snippetIndex => () => {
+        console.log('setSnippet',snippetIndex);
+        setSnippet(SNIPPETS[snippetIndex]);
+        setGameState({...gameState,startTime:new Date().getTime()})
+    };
     return (
         <div>
             <h2>Type Race</h2>
+            <hr/>
+            <h3>Snippet</h3>
+            {snippet}
+            <h4>{gameState.victory? `Done! Time: ${gameState.endTime}ms`: null}</h4>
             <input value={userText} onChange={updateUserText}/>
+            <hr/>
+            {
+                SNIPPETS.map((SNIPPET,index) =>(
+                    <button onClick = {chooseSnippet(index)} key = {index}>
+                        {SNIPPET.substring(0,10)}...
+                    </button>
+                ))
+            }
         </div>
-    )
+    );
+
 }
 
 
